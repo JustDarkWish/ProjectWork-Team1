462a577aaa4cfc8887b71f989345891e93a33bce
JustDarkWish <152283283+JustDarkWish@users.noreply.github.com>
2024-05-20 15:29:01 +0200

    Cambiato il login.js

--- JS/login.js
+++ JS/login.js
@@ -1,43 +1,50 @@
-let nameUtente= document.querySelector('#name');
-let surname= document.querySelector('#surname');
-let email= document.querySelector('#email');
-let password= document.querySelector('#password');
-let ruolo= document.querySelector('#ruolo');
-let btn= document.querySelector('.btn');
-let btnLogout= document.querySelector('.btnLogout');
+let nameUtente = document.querySelector('#name');
+let surname = document.querySelector('#surname');
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+let ruolo = document.querySelector('#ruolo');
+let btn = document.querySelector('.btn');
+let btnLogout = document.querySelector('.btnLogout');
+const form = document.getElementById('loginForm');
 
-const controlloPassword =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&]{8,}$/i;
+const controlloPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&]{8,}$/i;
 
-function login() {
-    if (nameUtente != '' && surname != '' && password != '' && email != '') {
-        localStorage.setItem('nome',nameUtente);
-        localStorage.setItem('cognome',surname);
-        localStorage.setItem('email',email);
-        localStorage.setItem('password',password);
-        localStorage.setItem('ruolo',ruolo);
-    }else{
+function login(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    if (nameUtente.value !== '' && surname.value !== '' && password.value !== '' && email.value !== '') {
+        if (controlloPass()) {
+            localStorage.setItem('nome', nameUtente.value);
+            localStorage.setItem('cognome', surname.value);
+            localStorage.setItem('email', email.value);
+            localStorage.setItem('password', password.value);
+            localStorage.setItem('ruolo', ruolo.value);
+        } else {
+            alert('Password non valida. Riprova.');
+        }
+    } else {
         alert('I dati inseriti non sono corretti, riprova.');
-        event.preventDefault();
     }
 }
+
 function logout() {
     localStorage.clear();
 }
+
 function controlloPass() {
     if (password.value.match(controlloPassword)) {
-        localStorage.setItem("password",password)
-        return true
-    }else{
-        document.getElementById("erroriPassword").innerHTML=`<ul class="text-danger">
+        return true;
+    } else {
+        document.getElementById("erroriPassword").innerHTML = `<ul class="text-danger">
         <li class="point">La tua password deve contenere:</li>
         <li>Almeno 8 caratteri</li>
         <li>Almeno una maiuscola</li>
         <li>Almeno una minuscola</li>
         <li>Almeno un numero</li>
         <li>Almeno un carattere speciale</li></ul>`;
-        return false
+        return false;
     }
 }
 
-btn.addEventListener('click',login);
-btnLogout.addEventListener('click',logout);
+form.addEventListener('submit', login);
+btnLogout.addEventListener('click', logout);
