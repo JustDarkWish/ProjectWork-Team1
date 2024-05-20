75602d6258cb3368bb60cd927063ea69bf886580
JustDarkWish <152283283+JustDarkWish@users.noreply.github.com>
2024-05-20 15:10:17 +0200

    Cambiato messaggio di errore nel login

--- JS/login.js
+++ JS/login.js
@@ -9,14 +9,14 @@ let btnLogout= document.querySelector('.btnLogout');
 const controlloPassword =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&]{8,}$/i;
 
 function login() {
-    if (nameUtente!= '' && surname!=''&& password!=''&& email!='') {
+    if (nameUtente != '' && surname != '' && password != '' && email != '') {
         localStorage.setItem('nome',nameUtente);
         localStorage.setItem('cognome',surname);
         localStorage.setItem('email',email);
         localStorage.setItem('password',password);
         localStorage.setItem('ruolo',ruolo);
     }else{
-        alert('Non hai inserito i valori corretti');
+        alert('I dati inseriti non sono corretti, riprova.');
         event.preventDefault();
     }
 }
