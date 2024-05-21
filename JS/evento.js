3bf259b9739ab8ebf2be3c758b1f682a9fc5ba97
8nopanda <01youssef.fakir@gmail.com>
2024-05-21 14:59:43 +0200

    navbarsbagliataindex

--- JS/evento.js
+++ JS/evento.js
@@ -1,1 +1,4 @@
-let img= document.querySelector
+let img= document.querySelector('#img');
+let description= document.querySelector('#descrizioneEvento');
+let btnAggiungi= document.querySelector('#btnCarello');
+let btnBack=document.querySelector('#btnBack');
