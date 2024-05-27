42ee14cdae7bd4b4497b9de9f2f98fa92ba5cd9a
hernan-manfredi <manfredirho@gmail.com>
2024-05-27 12:13:30 +0200

    Intervento Allen

--- eventia/src/main/java/com/eventia/demo/restcontroller/UtenteController.java
+++ eventia/src/main/java/com/eventia/demo/restcontroller/UtenteController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/utente")
 public class UtenteController {
