30350fe16616d10f3144945067c90a15e0366535
8nopanda <01youssef.fakir@gmail.com>
2024-05-28 12:11:14 +0200

    modifiche

--- JS/carrello.js
+++ JS/carrello.js
@@ -4,62 +4,93 @@ let checkboxDelete = document.querySelector('#checkboxDelete');
 let calcoloTotale = document.querySelector('#calcoloTotale');
 
 
+let utenteId = localStorage.getItem('UtenteId');
+
 let carrello = JSON.parse(localStorage.getItem('carrello'));
 console.log(carrello);
 
 
 popolaCarrello();
 
-function popolaCarrello(){
+function popolaCarrello() {
+    listaProdotti.innerHTML = '';
     carrello.forEach(elemento => {
-        listaProdotti.innerHTML += `<li class="d-flex justify-content-between align-items-start mb-3" carrello_id="${elemento.carrello_id}"> <span> <img src="${elemento.thumbnail}" class="img-fluid" pe-0" style="width: 25%" id="imgCart"> ${elemento.title} - ${elemento.price}€ <button class="btn btn-lg btnElimina"> </button></span></li>
-        `; 
+        listaProdotti.innerHTML += `<div class="swiper-slide">
+        <div class="card">
+        <img src="${elemento.locandinaURL}" alt="">
+        <div class="card-body d-flex flex-column">
+        <h5 id="nomeSkincare" class="card-title">${elemento.titolo}</h5>
+        <p id="brandSkincare" class="card-text">Numero di posti: ${elemento.posti}</p>
+        <p id="descrizioneSkincare" class="card-text">${elemento.descrizione}</p>
+        <p id="descrizioneSkincare" class="card-text">${elemento.luogoEvento}</p>
+        <p id="data-${elemento.eventoId}" class="card-text">${elemento.dataEvento}</p>
+        <p id="prezzo-${elemento.eventoId}" class="card-text">${elemento.prezzoListino}</p>
+        <button class="btnElimina" id="${elemento.eventoId}" type="button" class="btn btn-primary mt-auto" data-bs-toggle="modal" data-bs-target="#prodotto1Modal">elimina dal carrello</button>
+        <button class="btnAggiungi" id="${elemento.eventoId}" type="button" class="btn btn-primary mt-auto" data-bs-toggle="modal" data-bs-target="#prodotto1Modal">effetua ordine</button>
+        </div>
+        </div>
+        </div>`;
     });
-    
-    let btnElimina = document.querySelectorAll('.btnElimina');
-    
-    [...btnElimina].forEach(btnElimina => {
-        btnElimina.addEventListener('click', eliminaProdotto);
-    })
-    
+    document.querySelectorAll('.btnAggiungi').forEach(btn => {
+        btn.addEventListener('click', aggiungi);
+    });
+    document.querySelectorAll('.btnElimina').forEach(btnElimina => {
+        btnElimina.addEventListener('click', elimina);
+    });
     calcolaTotale();
 }
+class Prenotazione {
+    constructor(prezzoVendita, dataPrenotazione, quantitaPosti) {
+        this.prezzoVendita = prezzoVendita;
+        this.dataPrenotazione = dataPrenotazione;
+        this.quantitaPosti = quantitaPosti;
+    }
+}
 
-function calcolaTotale(totale = 0){
-    
-    carrello.forEach(elemento =>{
+function aggiungi() {
+    let prodottoId = event.target.getAttribute('id');
+    let prezzoVendita = document.getElementById("prezzo-" + prodottoId);
+    let dataPrenotazione = document.getElementById("data-" + prodottoId);
+    let quantitaPosti = 1;
+
+    const dateString = dataPrenotazione.innerHTML;
+    console.log(dateString);
+   
+    moment();
+    dataPrenotazione = moment(dateString, "YYYY-MM-DD").toDate();
+    // const year=
+    // const month=
+    // const day=
+    console.log(dataPrenotazione);
+
+
+    prezzoVendita = parseInt(prezzoVendita.innerHTML);
+    let urlAggiungi = `http://localhost:8080/api/prenotazione/utente/${utenteId}/evento/${prodottoId}`
+    let prenotazione = new Prenotazione(prezzoVendita.value, dataPrenotazione, quantitaPosti);
+    fetch(urlAggiungi, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(prenotazione),
+    });
+}
+
+function calcolaTotale(totale = 0) {
+    carrello.forEach(elemento => {
         totale += elemento.price;
         calcoloTotale.innerHTML = totale;
     })
 }
 
-
-function eliminaProdotto(event){
-    console.log(event.target); 
-    
-    let parent = this.parentNode;
-    let idRiga = parent.getAttribute("carrello_id");
-    console.log(idRiga);
-    
-    console.log(this);
-    
-    this.parentNode.remove();
-    
-    cercaProdottoLocalStorage(idRiga);
-
-    calcolaTotale();
-
-    calcoloTotale.innerHTML ='';
-}
-
-
-function cercaProdottoLocalStorage(idRiga){
-
-    let elFiltrati = carrello.filter((el) => idRiga != el.carrello_id);
-
-    console.log(elFiltrati);
-
-    localStorage.setItem('arrayCarrello', JSON.stringify(elFiltrati)); 
-
-    carrello = elFiltrati; 
+function elimina() {
+    event.preventDefault();
+    let prodottoId = event.target.getAttribute('id');
+    let urlElimina = `http://localhost:8080/api/prenotazione/utente/${utenteId}/evento/${prodottoId}`;
+    fetch(urlElimina, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
 }
--- eventia/src/main/java/com/eventia/demo/restcontroller/PrenotazioneController.java
+++ eventia/src/main/java/com/eventia/demo/restcontroller/PrenotazioneController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.eventia.demo.service.EventoService;
 import com.eventia.demo.service.PrenotazioneService;
 import com.eventia.demo.service.UtenteService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/prenotazione")
 public class PrenotazioneController {
